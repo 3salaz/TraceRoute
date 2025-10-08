cbb857879757f2d4cea3cd92f415844c7ceed18b
Erik <3salaz.dev@gmail.com>
2025-10-08 05:12:32 +0000

    wip: frontend integration working, need to debug server connection on desktop

--- trace-route/client/src/App.jsx
+++ trace-route/client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ScanInput from './components/ScanInput';
 
 function App() {
 
   return (
     <div className="bg-blue-500 w-full h-screen flex items-center justify-center">
-      <h1>testing tailwind css</h1>
+      <ScanInput/>
     </div>
   )
 }
