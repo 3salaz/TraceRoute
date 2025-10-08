73d5060ddd0f37ebb9932de54e8a1e9649a07673
Erik <3salaz.dev@gmail.com>
2025-10-08 07:04:08 +0000

    fixed server issue for port forwarding

--- trace-route/server/server.js
+++ trace-route/server/server.js
@@ -13,6 +13,6 @@ app.get('/api/health', (req, res) => {
 app.use("/api/scan", scansRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
